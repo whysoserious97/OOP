883a12988cdd9824dedbbeab5bd731ee580eefab
Lesco Andrei <53511833+whysoserious97@users.noreply.github.com>
2019-10-11 13:06:49 +0300

    Add files via upload
    Added comments to the code

--- Labs/Lab4/Main.java
+++ Labs/Lab4/Main.java
@@ -6,28 +6,28 @@ import java.util.Scanner;
 public class Main {
     public static void main (String[] args) {
 
-        Mystack mystack = new Mystack (100);
+        Mystack mystack = new Mystack (100);   // creates a stack with size 100
         String str; // declaring the expresion string
-        try {
+        try {     // try...catch block for reading files
 
-            File f = new File ("src\\Labs\\Lab4\\one_expression.txt");
-            Scanner scanner = new Scanner (f);
-            str=scanner.nextLine ();
+            File f = new File ("src\\Labs\\Lab4\\one_expression.txt");    // creating the variable with the first string
+            Scanner scanner = new Scanner (f);   // reading from the file and putting everything in the scanner
+            str=scanner.nextLine ();      // reads the line from scanner
             mystack.valid (str); // checks if the string has enclosed all parentheses
 
             System.out.println ();  // separates files expressions with a blank line
 
-            f = new File ("src\\Labs\\Lab4\\three_expressions.txt");
+            f = new File ("src\\Labs\\Lab4\\three_expressions.txt");    // reassign the source of the file
             scanner = new Scanner (f);
 
             for (int i=0;i<3;i++) //checks all 3 expresion from file
             {
-                str=scanner.nextLine ();
-                mystack.valid (str);
+                str=scanner.nextLine ();  // string to be checked
+                mystack.valid (str);      // executes the valid method that will print the result
             }
         }catch (Exception e)
         {
-           e.printStackTrace ();
+           e.printStackTrace (); // if there is no file the exception will occur and the StackTrace will be printed
         }
 
     }
--- Labs/Lab4/Mystack.java
+++ Labs/Lab4/Mystack.java
@@ -1,51 +1,58 @@
 package Labs.Lab4;
 
+import java.util.Arrays;
+
 public class Mystack {
-    char[] stack;
-    int top=0;
+    char[] stack;    // our parantheses will be stored in a char stack
+    int top=0;       // remember the indice where the last item is in the array
     Mystack(int size)
     {
-        stack= new char[size];
+        stack= new char[size];   // creating an array of chars
     }
 
     void push(char a)
     {
-        stack[++top] = a;
+        stack[++top] = a;    // it is
     }
-   public void pop()
+   public int  pop()
     {
-        if (top == -1)
+        if (top == -1)   // to avoid some special cases     )( ,   (())()       as valid
         {
-            System.out.println ("expression is invalid");
-            System.exit (0);
+            return 0; // expresion is invalid
         }
         else
         {
-            top--;
+            stack[top--]=0;     // char null has value 0 so we clear the slot and then top value decrease by 1
         }
+        return 1; //didnt found any special cases
     }
-    void find_top()
+    void find_top()   // this function is called to see if evey parentheses has a pair
     {
-        if (top == -1)
+        if (top == -1)     //  -1 shows that it is empty
             System.out.println ("expression is valid");
         else
             System.out.println ("expression is invalid");
     }
     void valid(String str)
     {
-        top=-1;
+        top=-1; // stack starts as empty
         int i;
-        for (i = 0;i < str.length ();i++)
+        for (i = 0;i < str.length ();i++) // going through string
         {
-            if (str.charAt (i) == '(')
+            if (str.charAt (i) == '(')   // when finds an '(' adds it in the stack
             {
-                push(str.charAt (i));
+                push(str.charAt (i));   // push in the stack
             }
             else if (str.charAt (i) == ')')
             {
-                pop();
+                if (pop()==0) // set the last slot as null and decrese the top value   // 0 means 100% invalid, and prints it
+                {
+                    System.out.println ("expression is invalid");
+                    return; // no need to check and we stop the function
+                }
+
             }
         }
-        find_top();
+        find_top(); // if there was no negative values for top it will reach this function to check if the stack is empty
     }
 }
