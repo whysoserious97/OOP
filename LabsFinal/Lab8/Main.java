779e6d08a2e5fb6111bf8303a5c63edeae59eecd
Lesco Andrei <53511833+whysoserious97@users.noreply.github.com>
2019-11-29 18:17:13 +0200

    Add files via upload

--- LabsFinal/Lab8/Main.java
+++ LabsFinal/Lab8/Main.java
@@ -20,15 +20,6 @@ public class Main extends Application {
         primaryStage.setTitle( "Title of the Window" );
         TextField textField1= new TextField( "Label 1" );
         TextField textField2 = new TextField( "Label 2" );
-        textField2.setOnAction( event -> {
-            if(textField2.getText()=="13"){
-                try {
-                    throw new UnluckyException();
-                } catch (UnluckyException e) {
-                    AlertBox.display( "UnluckyException","Your result will be unlucky" );
-                }
-            }
-        } );
         Label result = new Label( "Result" );
 
         Button button = new Button( "Divide");
@@ -51,23 +42,23 @@ public class Main extends Application {
     }
     public static void divideLabels( TextField dividend,TextField divisor,Label result ){
         try {
-            Double nr1 = Double.parseDouble( dividend.getText() );
-            Double nr2 = Double.parseDouble( divisor.getText() );
-            Double r = nr1/nr2;
+            Integer nr1 = Integer.parseInt( dividend.getText() );
+            Integer nr2 = Integer.parseInt( divisor.getText() );
+            Integer r = nr1/nr2;
             result.setText( r.toString() );
             if (nr2==13){
                 throw new  UnluckyException();
             }
         }catch (ArithmeticException ae)
         {
-            System.out.println("You cannot devide by 0");
+            AlertBox.display("Division to 0","You cannot devide by 0");
         }catch (NumberFormatException ne){
             AlertBox.display( "Exception catched","Your input is invalid to be parsed!!!" );
         }catch(UnluckyException ue)
         {
             AlertBox.display("Unlucky Exception","You are so unlucky right now!");
         }catch (Exception e){
-            System.out.println("This guy found an unexpected exception :)");
+            AlertBox.display("HOW DARE YOU TO FIND AN ERROR IN MY CODE??","This guy found an unexpected exception :)");
         }
     }
 
